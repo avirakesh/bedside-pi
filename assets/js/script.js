f1fb8fc0df64e03c541fda6214601c10dd53baff
Avichal Rakesh <avichal.rakesh@gmail.com>
2024-10-06 14:29:58 -0700

    run formatter on assets and views

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,11 @@
 /*
-* ----------------------------------------------------------------------------
-* "THE BEER-WARE LICENSE" (Revision 42):
-* AvichalRakesh wrote this file.  As long as you retain this notice you
-* can do whatever you want with this stuff. If we meet some day, and you think
-* this stuff is worth it, you can buy me a beer in return.   Avichal Rakesh
-* ----------------------------------------------------------------------------
-*/
-
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * AvichalRakesh wrote this file.  As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return.   Avichal Rakesh
+ * ----------------------------------------------------------------------------
+ */
 
 var h = -1;
 var m = -1;
@@ -21,201 +20,221 @@ var lastWeather = 0;
 var clock24hrs = false; /* Change to true for 24 hr clock */
 
 var days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+var months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+];
 
 $(function () {
-	$('.weather-container').click(function() {
-		updateWeather();
-	});
+    $(".weather-container").click(function () {
+        updateWeather();
+    });
 
-	if (clock24hrs) {
-		$('#am-pm').hide();
-	}
-	startTime();
+    if (clock24hrs) {
+        $("#am-pm").hide();
+    }
+    startTime();
 });
 
 function startTime() {
-	var today = new Date();
+    var today = new Date();
 
-	var hh = today.getHours();
-	var mm = today.getMinutes();
-	var ss = today.getSeconds();
+    var hh = today.getHours();
+    var mm = today.getMinutes();
+    var ss = today.getSeconds();
 
-	if (h != hh) {
-		h = hh;
-		if (!clock24hrs) {
-			hh = checkAMPM();
-			$('#hours').text(checkTime(hh));
-		} else {
-			$('#hours').text(checkTime(hh));
-		}
+    if (h != hh) {
+        h = hh;
+        if (!clock24hrs) {
+            hh = checkAMPM();
+            $("#hours").text(checkTime(hh));
+        } else {
+            $("#hours").text(checkTime(hh));
+        }
 
-		updateDate(today);
-	}
+        updateDate(today);
+    }
 
-	if (m != mm) {
-		m = mm;
-		$('#minutes').text(checkTime(m));
+    if (m != mm) {
+        m = mm;
+        $("#minutes").text(checkTime(m));
 
-	// To remove weather, comment out from here...
-	if (lastWeather == 0) {
-		updateWeather();
-	}
+        // To remove weather, comment out from here...
+        if (lastWeather == 0) {
+            updateWeather();
+        }
 
-	lastWeather = (lastWeather + 1) % weatherInterval;
-	// ... to here
-	}
+        lastWeather = (lastWeather + 1) % weatherInterval;
+        // ... to here
+    }
 
-	if (s != ss) {
-		s = ss;
-		$('#seconds').text(checkTime(s));
+    if (s != ss) {
+        s = ss;
+        $("#seconds").text(checkTime(s));
 
-		document.title = checkTime(h) + ':' + checkTime(m) + ':' + checkTime(s);
-	}
+        document.title = checkTime(h) + ":" + checkTime(m) + ":" + checkTime(s);
+    }
 
-	t = setTimeout(function() {
-		startTime();
-	}, 200);
+    t = setTimeout(function () {
+        startTime();
+    }, 200);
 }
 
-function checkAMPM () {
-	if (h >= 12) {
-		$('#am-pm').text("PM")
-		return h > 12 ? h - 12 : h;
-	} else {
-		$('#am-pm').text("AM")
-		return h == 0 ? 12 : h;
-	}
+function checkAMPM() {
+    if (h >= 12) {
+        $("#am-pm").text("PM");
+        return h > 12 ? h - 12 : h;
+    } else {
+        $("#am-pm").text("AM");
+        return h == 0 ? 12 : h;
+    }
 }
 
-function checkTime (i) {
-	return (i < 10) ? "0" + i : i;
+function checkTime(i) {
+    return i < 10 ? "0" + i : i;
 }
 
 function updateDate(today) {
-	var currentDate = today.getDate();
+    var currentDate = today.getDate();
 
-	if (date != currentDate) {
-		date = currentDate;
-		$('#date').text(checkTime(date));
+    if (date != currentDate) {
+        date = currentDate;
+        $("#date").text(checkTime(date));
 
-		var currentDay = today.getDay();
-		if (currentDay != day) {
-			day = currentDay;
-			setDay();
-		}
+        var currentDay = today.getDay();
+        if (currentDay != day) {
+            day = currentDay;
+            setDay();
+        }
 
-		var currentMonth = today.getMonth();
-		if (month != currentMonth) {
-			month = currentMonth;
-			setMonth();
-		}
+        var currentMonth = today.getMonth();
+        if (month != currentMonth) {
+            month = currentMonth;
+            setMonth();
+        }
 
-		var currentYear = today.getFullYear();
-		if (currentYear != year) {
-			year = currentYear;
-			$('#year').text(year);
-		}
-	}
+        var currentYear = today.getFullYear();
+        if (currentYear != year) {
+            year = currentYear;
+            $("#year").text(year);
+        }
+    }
 }
 
 function setDay() {
-	$('#day').text(days[day] + ",")
+    $("#day").text(days[day] + ",");
 }
 
 function setMonth() {
-	$('#month').text(months[month]);
+    $("#month").text(months[month]);
 }
 
 function updateWeather() {
-	$.ajax({
-		url: 'http://' + window.location.host + '/get-weather',
-		success: function (data) {
-			parseWeather(data);
-			$('.weather-container').css('visibility', 'visible');
-		},
-		error: function () {
-			$('.weather-container').css('visibility', 'hidden');
-		}
-	});
+    $.ajax({
+        url: "http://" + window.location.host + "/get-weather",
+        success: function (data) {
+            parseWeather(data);
+            $(".weather-container").css("visibility", "visible");
+        },
+        error: function () {
+            $(".weather-container").css("visibility", "hidden");
+        },
+    });
 }
 
 function parseWeather(data) {
-	if (data != '{}'){
-		// console.log(data);
-		$('.weather-summary-div').text(data['summary']);
+    if (data != "{}") {
+        // console.log(data);
+        $(".weather-summary-div").text(data["summary"]);
 
-		var timestamp = Math.round(Date.now() / 1000);
-		var isDay = timestamp > data['sunrise'] && timestamp < data['sunset'];
+        var timestamp = Math.round(Date.now() / 1000);
+        var isDay = timestamp > data["sunrise"] && timestamp < data["sunset"];
 
-		$('.weather-icon').html(generateIconSpan(data["icon"]));
+        $(".weather-icon").html(generateIconSpan(data["icon"]));
 
-		if (data['precipitation']) {
-			var probability = data['precipitation']['probability']
-			var amount = data['precipitation']['amount'];
-			$('.preci-span').text(probability + '% ');
-			$('.preci-div').show()
-		} else {
-			$('.preci-div').hide()
-		}
+        if (data["precipitation"]) {
+            var probability = data["precipitation"]["probability"];
+            var amount = data["precipitation"]["amount"];
+            $(".preci-span").text(probability + "% ");
+            $(".preci-div").show();
+        } else {
+            $(".preci-div").hide();
+        }
 
-		$('.temp-span').text(data['temp']);
-		$('.feels-like-span').text(data['appTemp']);
+        $(".temp-span").text(data["temp"]);
+        $(".feels-like-span").text(data["appTemp"]);
 
-		imgUrl = mapWeatherIdToBackgroundImage(data["icon"], isDay);
+        imgUrl = mapWeatherIdToBackgroundImage(data["icon"], isDay);
 
-		if ($('.shown').attr('src') != imgUrl) {
-			$('.hidden').attr('src', imgUrl);
-			$('.background-img').toggleClass('hidden');
-			$('.background-img').toggleClass('shown');
-		}
+        if ($(".shown").attr("src") != imgUrl) {
+            $(".hidden").attr("src", imgUrl);
+            $(".background-img").toggleClass("hidden");
+            $(".background-img").toggleClass("shown");
+        }
 
-		if (data['alerts']) {
-			var html = '';
-			for (var alert of data['alerts']) {
-				html += '<div>' +
-						'<span class="alert-sign">!</span>' +
-						'<span class="alert-text">' + alert + '</span>' +
-						'</div>';
-			}
-		    $('.alerts-div').html(html);
-		    $('.alerts-div').show();
-		} else {
-			$('.alerts-div').hide();
-			$('.alerts-div').html('');
-		}
-	}
-
+        if (data["alerts"]) {
+            var html = "";
+            for (var alert of data["alerts"]) {
+                html +=
+                    "<div>" +
+                    '<span class="alert-sign">!</span>' +
+                    '<span class="alert-text">' +
+                    alert +
+                    "</span>" +
+                    "</div>";
+            }
+            $(".alerts-div").html(html);
+            $(".alerts-div").show();
+        } else {
+            $(".alerts-div").hide();
+            $(".alerts-div").html("");
+        }
+    }
 }
 
 function generateIconSpan(icon, isDay) {
-	return '<i class="wi wi-darksky-' + icon + '"></i>';
+    return '<i class="wi wi-darksky-' + icon + '"></i>';
 }
 
 function mapWeatherIdToBackgroundImage(icon, isDay) {
-	var imgUrl = 'assets/images/weather/' + (isDay ? 'day/' : 'night/');
-	imgUrl += (icon + ".jpg");
-	return imgUrl
+    var imgUrl = "assets/images/weather/" + (isDay ? "day/" : "night/");
+    imgUrl += icon + ".jpg";
+    return imgUrl;
 }
 
 function updateNotif() {
-	$.ajax({
-		url: 'http://' + window.location.host + '/get-notifications.php',
-		success: function(data) {
-			// console.log(data);
-			parseNotif(data);
-		}
-	});
+    $.ajax({
+        url: "http://" + window.location.host + "/get-notifications.php",
+        success: function (data) {
+            // console.log(data);
+            parseNotif(data);
+        },
+    });
 }
 
-function parseNotif (data) {
-	//console.log("Data: " + data);
-	var html = '';
-	var time = Math.round(Date.now() / 1000);
-	for (var i = 0; i < data.length; i++) {
-		// console.log(data[i]['package']);
-		html += '<li class="notif-list-item"><img class="notif-image" src="assets/images/' + data[i]['package'] + '.png?' + time + '"></li>'
-	}
+function parseNotif(data) {
+    //console.log("Data: " + data);
+    var html = "";
+    var time = Math.round(Date.now() / 1000);
+    for (var i = 0; i < data.length; i++) {
+        // console.log(data[i]['package']);
+        html +=
+            '<li class="notif-list-item"><img class="notif-image" src="assets/images/' +
+            data[i]["package"] +
+            ".png?" +
+            time +
+            '"></li>';
+    }
 
-	$('.notif-list').html(html);
+    $(".notif-list").html(html);
 }
